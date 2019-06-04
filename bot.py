0c2e8b989cc5f87ffdbf4a1faadbe5c875d61088
Dejan <dejan.alvadzijevic@hotmail.rs>
2019-06-04 22:57:30 +0200

    specifying parser

--- bot.py
+++ bot.py
@@ -2,6 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 
 data = requests.get("https://poslovi.infostud.com/oglasi-za-posao/beograd?category%5B0%5D=5&dist=50&vreme_postavljanja=today")
-soup = BeautifulSoup(data.text)
+soup = BeautifulSoup(data.text,'html.parser')
 print("document",soup)
 print("hello bot")
