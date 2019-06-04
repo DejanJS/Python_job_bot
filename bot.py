dd519333e615bb2e1cd0592dfb3a17dbdc0145d6
Dejan <dejan.alvadzijevic@hotmail.rs>
2019-06-04 22:51:53 +0200

    fetch/scrape document

--- bot.py
+++ bot.py
@@ -1,1 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+data = requests.get("https://poslovi.infostud.com/oglasi-za-posao/beograd?category%5B0%5D=5&dist=50&vreme_postavljanja=today")
+soup = BeautifulSoup(data.text)
+print("document",soup)
 print("hello bot")
