fe21cdc17fa037cf2a93f75f4dca658169defd09
Dejan <dejan.alvadzijevic@hotmail.rs>
2019-06-06 22:56:55 +0200

    job titles

--- bot.py
+++ bot.py
@@ -3,5 +3,8 @@ import requests
 
 data = requests.get("https://poslovi.infostud.com/oglasi-za-posao/beograd?category%5B0%5D=5&dist=50&vreme_postavljanja=today")
 soup = BeautifulSoup(data.text,'html.parser')
-print("document",soup)
-print("hello bot")
+job_titles = []
+for i in soup.select("h2.uk-margin-remove"):
+	job_titles.append(i['title'])
+
+print("job titles",job_titles)
