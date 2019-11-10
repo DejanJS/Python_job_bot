1e5225b63aa1d4760e0c4e724fe6d7047125f8d4
Dejan <dejan.alvadzijevic@hotmail.rs>
2019-11-11 00:07:53 +0100

    cleaning

--- bot.py
+++ bot.py
@@ -62,20 +62,3 @@ def get_interests():
 main_thread = threading.Thread(target=scrape,args=("https://poslovi.infostud.com/oglasi-za-posao/beograd?category%5B0%5D=5&dist=50",get_interests()))
 
 main_thread.start()
-"""
-data = requests.get("https://poslovi.infostud.com/oglasi-za-posao/beograd?category%5b0%5d=5&dist=50&vreme_postavljanja=today")
-soup = beautifulsoup(data.text,'html.parser')
-#job_titles = list(map(lambda item:item['title'],soup.select("h2.uk-margin-remove")))
-# job_titles = [item['title'] for item in soup.select("h2.uk-margin-remove")]
-posts = soup.findall('div',id=re.compile("oglas_[0-9]*"))
-jobs = [jobfinder(post) for post in posts]
-#print('test',jobs)
-#print('posts',[post.title for post in jobs])
-interestedin = ['react','javascript','node','python']
-interests = [job.interested(interestedin) for job in jobs]
-interested_jobs = list(filter(lambda job : job.interested(interestedin),jobs))
-interested_links = [interested.link for interested in interested_jobs]
-print("python jobs links : ",interested_links)
-#print('intrests',interests)
-print("job titles",[post.title for post in jobs])
-"""
